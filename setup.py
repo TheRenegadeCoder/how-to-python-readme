d6cdc8e014c5fc6b6013a870fbb88ebacf422b87
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-08 16:47:36 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yomu",
+    version="0.1.0",
+    author="The Renegade Coder",
+    author_email="jeremy.grifski@therenegadecoder.com",
+    description="Generates the README for the 'How to Python Code' repo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheRenegadeCoder/how-to-python-readme",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "feedparser>=6",
+        "beautifulsoup4>=4",
+        "requests>=2",
+        "SnakeMD>=0"
+    ],
+    entry_points={
+        "console_scripts": [
+            'readme = yomu.readme:main'
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
